7e8aed9e00ed2dec752492feda4e4621004edc3a
Michal_Parchanowicz <michal.parchanowicz@gmail.com>
2024-02-01 10:11:29 +0100

    feat: mongo tests

--- API/MongoDB/app/models/categoriesModel.js
+++ API/MongoDB/app/models/categoriesModel.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const categorySchema = mongoose.Schema({
+    categoryId: {
+        type: Number,
+        require: [true, "Category id is required"],
+    },
     name: {
         type: String,
         require: [true, "Category name is required"],
